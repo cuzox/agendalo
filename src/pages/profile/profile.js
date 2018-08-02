0f844da3b466a3f5dcfb8b11858a72140f306df3
cuzox <calevime@gmail.com>
2018-08-02 00:09:10 -0400

    Prop mistake

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -65,7 +65,7 @@ class Profile extends Component {
           </div>
           <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginLeft: "20px"}}>
             <span style={{fontSize: "16px", padding: "8px"}} >
-              <b>Agendadas: {this.props.scheduledActivities.length}</b>
+              <b>Agendadas: {this.props.scheduled.length}</b>
             </span>
             <span style={{fontSize: "16px", padding: "8px"}}>
               <b>Publicadas: {this.props.myActivities.length}</b>
